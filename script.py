7dbeb926d0b72f80bf1f0f1eca50c0afcc7ec3cd
BadrBelkadi <Badrdinbelkadi@gmail.com>
2021-06-05 13:06:38 +0100

    Initial Commit

---
+++ script.py
@@ -0,0 +1,46 @@
+import webbrowser
+import time
+import os
+
+# Path for the apps to run
+# Make sure you replace them with the path of the path you want to run
+To_do_List = r'C:\Users\"Badreddine Belkadi"\AppData\Local\Programs\todoist\Todoist.exe'
+
+Vs_Code = r'C:\Users\"Badreddine Belkadi"\AppData\Local\Programs\"Microsoft VS Code"\Code.exe'
+
+cmd = r'%windir%\system32\cmd.exe'
+
+# Commands to run in CMD
+run_td = f'start {To_do_List}'
+run_vs = f'start {Vs_Code}'
+run_cmd = f'start {cmd}'
+
+# Running the commands
+os.system(run_vs)
+time.sleep(1)
+os.system(run_cmd)
+time.sleep(1)
+os.system(run_td)
+time.sleep(1)
+
+# Websites to open on the browser
+# Add other websites you use on your daily routine
+Google = 'https://google.com/'
+E_mail = 'https://mail.google.com/mail/'
+Github = 'https://github.com/'
+Stackoverflow = 'https://stackoverflow.com'
+Spotify = 'https://open.spotify.com/'
+
+
+# Opening the websites
+webbrowser.open(Google)
+time.sleep(1)
+webbrowser.open_new_tab(E_mail)
+time.sleep(1)
+webbrowser.open_new_tab(Github)
+time.sleep(1)
+webbrowser.open_new_tab(Stackoverflow)
+time.sleep(1)
+webbrowser.open_new_tab(Spotify)
+
+# Open other websites using  webbrowser.open(Your_website)
